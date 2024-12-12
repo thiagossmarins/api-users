6823406101c1045e842e5fd9b5ebe58bf821ebbb
Thiago <thiagodesouzasallesmarins@gmail.com>
2024-12-12 18:51:42 -0300

    criando rotas principais, get e post

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+let usuarios = [];
+
+app.get('/usuarios', (req, res) => {
+  res.status(200).json(usuarios);
+});
+
+app.post('/usuarios', (req, res) => {
+  usuarios.push(req.body);
+
+  res.status(201).json(req.body);
+});
 
 app.listen(3000);
